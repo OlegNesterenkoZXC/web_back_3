eb59ef40613a04606225ec932be9e69f0c480123
Oleg <olegik-n@yandex.com>
2022-05-05 20:06:22 +0300

    Added user authentication

--- formlogin.php
+++ formlogin.php
@@ -12,21 +12,21 @@
 <body>
 
 	<div class="form">
-		<div class="form__header">
-			<div class='form__contaner'>
-				<span class='form__span form__span_header'>Авторизируйтесь</span>
-			</div>
-		</div>
+		<?php
+		echo $loginHeader;
+		?>
 		<form class="form__body" action=".\login.php" method="post">
 			<div class="form__item">
 				<label class="form__label">
 					<input class="form__input form__input_text" placeholder="Логин" type="text" name="login">
 				</label>
+				<?php echo $message['login-error']; ?>
 			</div>
 			<div class="form__item">
 				<label class="form__label">
 					<input class="form__input form__input_text" placeholder="Пароль" type="password" name="password">
 				</label>
+				<?php echo $message['password-error']; ?>
 			</div>
 			<div class="form__item form__item_submit">
 				<label class="form__label">
