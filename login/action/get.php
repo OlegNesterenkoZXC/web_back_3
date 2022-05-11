e291d4cdb04a84a7ddedd487c8397b7459c971bf
Oleg <olegik-n@yandex.com>
2022-05-11 17:11:56 +0300

    The layout with the login has been rebuilt

--- login/action/get.php
+++ login/action/get.php
@@ -8,46 +8,37 @@ if (!empty($_COOKIE[session_name()]) && !empty($_SESSION['login'])) {
 	exit();
 }
 
+require_once(BASE_DIR . "login/layout/start.php");
+
 if (!empty($_COOKIE['login-request-error'])) {
 	setcookie("login-request-error", '', time() - 60 * 60 * 24);
-	$loginHeader =
-		"<div class='form__header'>
-				<div class='form__contaner form__container_err'>
-					<span class='form__span'>Что-то пошло не так! =(</span>
-				</div>
-			</div>";
+
+	require_once(BASE_DIR . "login/layout/header/error.php");
 } elseif (!empty($_COOKIE['login-auth-error'])) {
 	setcookie('login-auth-error', '', time() - 60 * 60 * 24);
-	$loginHeader =
-		"<div class='form__header'>
-				<div class='form__contaner form__container_err'>
-					<span class='form__span'>Неверный логин и/или пароль!</span>
-				</div>
-			</div>";
+
+	require_once(BASE_DIR . "login/layout/header/dataError.php");
 } else {
-	$loginHeader =
-		"<div class='form__header'>
-				<div class='form__contaner'>
-					<span class='form__span form__span_header'>Авторизируйтесь</span>
-				</div>
-			</div>";
+	require_once(BASE_DIR . "login/layout/header/header.php");
 }
 
 $message = array('login-error' => '', 'password-error' => '');
 if (!empty($_COOKIE['login-error'])) {
+	setcookie('login-error', '', time() - 60 * 60 * 24);
+
 	$message['login-error'] =
 		"<div class='form__container form__container_err'>
-				<span class='form__span'>{$_COOKIE['login-error']}</span>
-			</div>";
-	setcookie('login-error', '', time() - 60 * 60 * 24);
+			<span class='form__span'>{$_COOKIE['login-error']}</span>
+		</div>";
 }
 
 if (!empty($_COOKIE['password-error'])) {
+	setcookie('password-error', '', time() - 60 * 60 * 24);
 	$message['password-error'] =
 		"<div class='form__container form__container_err'>
-				<span class='form__span'>{$_COOKIE['password-error']}</span>
-			</div>";
-	setcookie('password-error', '', time() - 60 * 60 * 24);
+			<span class='form__span'>{$_COOKIE['password-error']}</span>
+		</div>";
 }
 
 require_once(BASE_DIR . "login/layout/form.php");
+require_once(BASE_DIR . "login/layout/end.php");
